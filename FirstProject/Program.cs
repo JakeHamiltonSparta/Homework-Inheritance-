c963708b44755c7e297b70ee3e522b440cd470f4
Jake Hamilton <jhamilton@spartaglobal.com>
2019-10-29 08:53:03 +0000

    This is my homework

---
+++ FirstProject/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using FirstProject2;
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coach jake = new Coach();
+        }
+
+    }
+}
+
+namespace FirstProject2
+{
+            
+    class Competitor
+    {
+        private int age;
+        public string firstName;
+        public string lastName;
+        public string inGameName;
+
+    }
+    class Coach : Competitor
+    {
+        public string previousTeams;
+    }
+    class Player : Competitor
+    {
+        public string previousPlacements;
+        public string location;
+        public bool ableToTravel;
+        
+    }
+    class FreeAgents : Player
+    {
+        public string previousEarnings;
+    }
+
+    class Wins
+    {
+        private int wins;
+    }
+
+}
+
+
